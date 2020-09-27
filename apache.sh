062ec896ae60df8f8811bc976e9927ff4d175ffc
Ajay <ajayswal.adm@gmail.com>
2020-09-27 01:53:14 -0500

    add coment to remove apche2 from ubuntu

--- apache.sh
+++ apache.sh
@@ -8,3 +8,7 @@ cd /var/www/html
 sudo rm index.html
 sudo echo "<html><h1>This My Apache Server</h1></html>" > index.html
 curl -4 icanhazip.com
+#  to remove apache2 completely from the ubuntu server command.Remove apache2 packages and dependencies
+# sudo apt-get purge apache2 apache2-utils apache2.2-bin
+#Check what's left:
+#whereis apache2
