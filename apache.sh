d6eec2ded6c941f7d247e666c630a335d19c7808
Ajay <ajayswal.adm@gmail.com>
2020-09-27 01:19:46 -0500

    remove defoult line

--- apache.sh
+++ apache.sh
@@ -5,5 +5,7 @@ sudo systemctl start apache2
 sudo systemctl status apache2
 sudo apt-get install curl
 cd /var/www/html
+sudo rm index.html
+sudo su
 echo "<html><h1>This My Apache Server</h1></html>" > index.html
 curl -4 icanhazip.com
