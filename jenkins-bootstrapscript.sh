c61c13020444dc4a50b338ae381a0e388083f03a
ajayswal <47310067+ajayswal@users.noreply.github.com>
2020-09-29 10:54:21 -0500

    Update jenkins-bootstrapscript.sh
    adit java staps

--- jenkins-bootstrapscript.sh
+++ jenkins-bootstrapscript.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt install openjdk-8-jre-headless -y
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install -y openjdk-8-jdk
 sudo apt-get update
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
