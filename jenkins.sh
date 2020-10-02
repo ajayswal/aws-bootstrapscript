1a6871325b7d6d35eedccc2e0032d4c385408edc
Ajay <ajayswal.adm@gmail.com>
2020-10-01 19:37:02 -0500

    testing face

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo add-apt-repository ppa:openjdk-r/ppa
+sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt-get update
 sudo apt-get install -y openjdk-8-jdk
 sudo apt-get update
@@ -9,8 +9,8 @@ echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/source
 sudo apt-get update
 sudo apt-get install jenkins -y --allow-unauthenticated
 sudo systemctl start jenkins
-sudo su
-echo "JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/profile
-echo "PATH=\$JAVA_HOME/bin:\$PATH" >> /etc/profile
-source /etc/profile  ## to reload the configuration
-exit
+#sudo su
+#echo "JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/profile
+#echo "PATH=\$JAVA_HOME/bin:\$PATH" >> /etc/profile
+#source /etc/profile  ## to reload the configuration
+#exit
